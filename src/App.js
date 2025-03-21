ad8109aff9083e1afe1f751ea49a1da7c0f66306
Abdur Razzaq <abdurrazaq789@gmail.com>
2025-03-21 16:40:10 +0500

    Lecture 06 | 18 Controlled Elements | Completed

--- src/App.js
+++ src/App.js
@@ -19,13 +19,20 @@ function Logo(){
 }
 function Form(){
   const [description, setDescription] = useState("")
-  const [quantity, setQuantity] = useState(5)
+  const [quantity, setQuantity] = useState(1)
   function handleSubmit(e){
     e.preventDefault()
+    if (!description) return(
+      alert("description cannot be empty")
+    );
+    const newItem = {description, quantity, packed: false, id: Date.now()}
+    console.log(newItem)
+    setDescription("")
+    setQuantity(1)
   }
   return <form className="add-form" onSubmit={handleSubmit}>
     <h3>What do you need for your 😍 trip?</h3>
-    <select value={quantity} onChange={(e)=> setQuantity(e.target.value)}>
+    <select value={quantity} onChange={(e)=> setQuantity(Number(e.target.value))}>
       {Array.from({length: 20}, (_, i)=> i+1).map((num)=> 
       <option key={num} value={num}>{num}</option>)}
     </select>
