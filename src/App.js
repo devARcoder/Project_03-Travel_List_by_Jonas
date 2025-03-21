0c5c685c2dc48435e73c32bc517f871e12b30a31
Abdur Razzaq <abdurrazaq789@gmail.com>
2025-03-21 16:25:25 +0500

    still continue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 3, packed: false },
+  { id: 3, description: "Charger", quantity: 3, packed: false },
 ];
 
 export default function App() {
@@ -16,16 +18,18 @@ function Logo(){
   return <h1>🌴 Far Away 💼</h1>
 }
 function Form(){
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(5)
   function handleSubmit(e){
     e.preventDefault()
   }
   return <form className="add-form" onSubmit={handleSubmit}>
     <h3>What do you need for your 😍 trip?</h3>
-    <select>
+    <select value={quantity} onChange={(e)=> setQuantity(e.target.value)}>
       {Array.from({length: 20}, (_, i)=> i+1).map((num)=> 
       <option key={num} value={num}>{num}</option>)}
     </select>
-    <input type="text" placeholder="Item..."/>
+    <input onChange={(e)=> setDescription(e.target.value)} value={description} type="text" placeholder="Item..."/>
     <button>Add</button>
   </form>
 }
