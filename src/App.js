a8e2bb3431ec7a3ee9a6b69048c13ffed5f3cc6d
Abdur Razzaq <abdurrazaq789@gmail.com>
2025-04-02 10:50:28 +0500

    Lecture 07 | 04 Thinking About State and Lifting State Up | Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 3, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Charger", quantity: 3, packed: false },
+// ];
 
 export default function App() {
+  const [items, setItems] = useState([])
+  function handleAddItems (item){
+    setItems((items)=> [...items, item])
+  }
   return <div className="app">
     <Logo/>
-    <Form/>
-    <PackingList/>
+    <Form onAddItems={handleAddItems}/>
+    <PackingList items={items}/>
     <Stats/>
   </div>
 }
 function Logo(){
   return <h1>🌴 Far Away 💼</h1>
 }
-function Form(){
+function Form({onAddItems}){
   const [description, setDescription] = useState("")
   const [quantity, setQuantity] = useState(1)
+  
+
+
   function handleSubmit(e){
     e.preventDefault()
     if (!description) return(
@@ -27,6 +34,7 @@ function Form(){
     );
     const newItem = {description, quantity, packed: false, id: Date.now()}
     console.log(newItem)
+    onAddItems(newItem)
     setDescription("")
     setQuantity(1)
   }
@@ -40,10 +48,10 @@ function Form(){
     <button>Add</button>
   </form>
 }
-function PackingList(){
+function PackingList({items}){
   return <div className="list">
     <ul>
-      {initialItems.map((item)=> <Item item={item} key={item.id}/>)}
+      {items.map((item)=> <Item item={item} key={item.id}/>)}
     </ul>
   </div>
 }
